972eb6f1491aef1261da63e288559827f80de4be
Alexander Thuo <technical@omnivoltaic.com>
2022-03-21 16:45:31 +0300

    fixed error

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AppPage from '../@crema/hoc/DefaultPage/index'
+import AppPage from '../@crema/hoc/DefaultPage/index';
 import asyncComponent from "../@crema/utility/asyncComponent";
 
 const SignIn = asyncComponent(() => import('../modules/auth/Signin/index'));
